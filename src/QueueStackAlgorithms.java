be88d1f0eacf6870417b3b3e135081885c55fe8d
Gabriel Hardy-Francon <ghfrancon@protonmail.com>
2022-01-31 14:11:26 +0100

    adding Queue and Stack algorithms

--- src/QueueStackAlgorithms.java
+++ src/QueueStackAlgorithms.java
@@ -63,6 +63,24 @@ public class QueueStackAlgorithms {
         }
     }
 
+    public boolean hasMatchingParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '(') {
+                stack.push(current);
+            } else if (current == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
 
 
